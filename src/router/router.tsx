a8e914a639b08f58e45bac0983f4655a257e6ebc
Sezer <s.y.sezeryardim@hotmail.com>
2024-09-06 16:42:40 +0300

    productlistview && productDetailsView

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import ProductListView from "../views/ProductListView";
+import ProductDetailsView from "../views/ProductDetailsView";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "",
+        element: <ProductListView />
+      },
+      {
+        path: "/:id",
+        element: <ProductDetailsView />
+      }
+    ]
   },
 ]);
---
+++ src/views/ProductDetailsView.tsx
@@ -0,0 +1,5 @@
+export default function ProductDetailsView(){
+    return (
+        <div>Product Details</div>
+    )
+}
