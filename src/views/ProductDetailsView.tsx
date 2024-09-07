3c0af5fa14c18f0e978a98413b36034d55a72db1
Sezer <s.y.sezeryardim@hotmail.com>
2024-09-07 20:12:34 +0300

    use components in views

--- src/views/ProductDetailsView.tsx
+++ src/views/ProductDetailsView.tsx
@@ -1,5 +1,22 @@
-export default function ProductDetailsView(){
-    return (
-        <div>Product Details</div>
-    )
+import { Grid2 as Grid } from "@mui/material";
+import Cart from "../components/Cart";
+import Checkout from "../components/Checkout";
+import ProductDetailsCard from "../components/ProductDetailsCard";
+export default function ProductDetailsView() {
+  return (
+    <Grid container columnGap={"30px"} wrap="nowrap">
+      <Grid size={10} container>
+        <ProductDetailsCard
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque temporibus, dolorem itaque necessitatibus, iure quo nostrum, asperiores eum dolor eveniet dolore illo. Velit, ratione! Amet quia laborum nobis placeat facilis.
+Tempore quo molestiae fugit ipsam quos dolores, recusandae exercitationem nam! Modi in omnis quae quisquam tenetur facilis sunt dicta explicabo animi dolores voluptatem voluptatibus praesentium earum eum, sapiente molestiae labore!"
+          name="Apple"
+          price={1100}
+        ></ProductDetailsCard>
+      </Grid>
+      <Grid size={2}>
+        <Cart name="Samsung s22" price="12.000₺" count={1}></Cart>
+        <Checkout price={11700}></Checkout>
+      </Grid>
+    </Grid>
+  );
 }
--- src/views/ProductListView.tsx
+++ src/views/ProductListView.tsx
@@ -1,5 +1,51 @@
-export default function ProductListView(){
-    return (
-        <div>ProductListView</div>
-    )
+import { Grid2 as Grid } from "@mui/material";
+import BrandsFilter from "../components/BrandsFilter";
+import Cart from "../components/Cart";
+import Checkout from "../components/Checkout";
+import ModelFilter from "../components/ModelFilter";
+import ProductCard from "../components/ProductCard";
+import SortByFilter from "../components/SortByFilter";
+
+export default function ProductListView() {
+  return (
+    <Grid container>
+      <Grid size={2}>
+        <SortByFilter></SortByFilter>
+        <BrandsFilter></BrandsFilter>
+        <ModelFilter></ModelFilter>
+      </Grid>
+      <Grid size={8} container rowSpacing={"26px"} columnGap={"30px"}>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+      </Grid>
+      <Grid size={2}>
+        <Cart
+         name="Samsung s22" price="12.000₺" count={1}></Cart
+        >
+        <Checkout price={11700}></Checkout>
+      </Grid>
+    </Grid>
+  );
 }
