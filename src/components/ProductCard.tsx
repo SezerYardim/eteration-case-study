3b8d6d04e8fd61de7d7a1c4ee2a4470e19786dfd
Sezer <s.y.sezeryardim@hotmail.com>
2024-09-07 17:59:17 +0300

    productcard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,21 +11,25 @@ import {
 export default function ProductCard() {
   return (
     <Card>
-      <CardMedia
-        component="img"
-        height={150}
-        width={150}
-        image=""
-        alt="Product_Image"
-      />
-      <CardContent>
-        <Typography></Typography>
-        <Typography></Typography>
-      </CardContent>
       <CardActionArea>
-        <CardActions>
-          <Button variant="contained">Add To Cart</Button>
-        </CardActions>
+        <CardMedia
+          component="img"
+          height={150}
+          width={150}
+          image=""
+          alt="Product_Image"
+        />
+        <CardContent>
+          <Typography></Typography>
+          <Typography></Typography>
+        </CardContent>
+        <CardActionArea>
+          <CardActions>
+            <Button sx={{ width: "100%" }} variant="contained">
+              Add To Cart
+            </Button>
+          </CardActions>
+        </CardActionArea>
       </CardActionArea>
     </Card>
   );
